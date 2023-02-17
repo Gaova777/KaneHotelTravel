1d093f72f676adb50a7e041046584e19d1e777d6
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2023-02-17 16:57:14 -0600

    HotelDetail - reducer & actions
    HotelDetail reducer & actions created

--- Client/src/redux/actions/index.ts
+++ Client/src/redux/actions/index.ts
@@ -1,35 +1,58 @@
-import data from "./data.json"
-import { Dispatch } from "redux"
+import data from "./data.json";
+import { Dispatch } from "redux";
+import axios from "axios";
 
 interface GetCitiesAction {
-    type: "GET_CITIES",
-    payload: any
+	type: "GET_CITIES";
+	payload: any;
 }
 
 interface GetActivitiesAction {
-    type: "GET_ACTIVITIES",
-    payload: any
+	type: "GET_ACTIVITIES";
+	payload: any;
 }
 
 interface GetHotelsAction {
-    type: "GET_HOTELS",
-    payload: any
+	type: "GET_HOTELS";
+	payload: any;
 }
-
-export type Action = GetCitiesAction | GetActivitiesAction | GetHotelsAction
+interface GetHotelDetail {
+	type: "GET_HOTEL_DETAIL";
+	payload: any;
+}
+export type Action =
+	| GetCitiesAction
+	| GetActivitiesAction
+	| GetHotelsAction
+	| GetHotelDetail;
 
 export const getCities = () => {
-            return async function(dispatch: Dispatch){
-                // fetch(data)
-                // .then(res => res.json())
-                // .then(json => dispatch({type: "GET_CITIES", payload: json.data}))
-                dispatch({type: "GET_CITIES", payload: data.results})
-            
-        }    
-    }
+	return async function (dispatch: Dispatch) {
+		// fetch(data)
+		// .then(res => res.json())
+		// .then(json => dispatch({type: "GET_CITIES", payload: json.data}))
+		dispatch({ type: "GET_CITIES", payload: data.results });
+	};
+};
 
 export const getHotels = () => {
-    return async function(dispatch: Dispatch){
-        dispatch({type: "GET_HOTELS", payload: data.results})
-    }
+	return async function (dispatch: Dispatch) {
+		dispatch({ type: "GET_HOTELS", payload: data.results });
+	};
+};
+
+export function getHotelDetail(id: any) {
+	return async function (dispatch: any) {
+		try {
+			var json = await axios.get(`http://localhost:3001/hotel/${id}`);
+			console.log(json.data);
+
+			return dispatch({
+				type: "GET_DETAILS",
+				payload: json.data,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 }
--- Client/src/redux/reducer/index.ts
+++ Client/src/redux/reducer/index.ts
@@ -1,27 +1,35 @@
-import { Action } from "../actions"
+import { Action } from "../actions";
 
-// creamos la interface InitState con los tipos de dato de cada propiedad del estado inicial 
+// creamos la interface InitState con los tipos de dato de cada propiedad del estado inicial
 
 const initialState = {
-    cities: [],
-    activities : []
-}
+	cities: [],
+	activities: [],
+	hotels: [],
+	hotelDetail: [],
+};
 
 const rootReducer = (state = initialState, action: Action) => {
-    switch(action.type){
-        case "GET_CITIES":
-            return{
-                ...state,
-                cities: action.payload
-            }
-        case "GET_HOTELS":
-            return{
-                ...state,
-                cities:action.payload
-            }
-        default:
-            return {...state}
-    }
-}
+	switch (action.type) {
+		case "GET_CITIES":
+			return {
+				...state,
+				cities: action.payload,
+			};
+		case "GET_HOTELS":
+			return {
+				...state,
+				cities: action.payload,
+			};
+		case "GET_HOTEL_DETAIL": {
+			return {
+				...state,
+				hotelDetail: { ...action.payload },
+			};
+		}
+		default:
+			return { ...state };
+	}
+};
 
-export default rootReducer
+export default rootReducer;
