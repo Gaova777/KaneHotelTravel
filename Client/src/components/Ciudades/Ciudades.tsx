2fa0cc3bda8b2ec38dd26a07736a8a649c35df7c
Andres Camilo Rios <acamilorm4556@gmail.com>
2023-02-17 10:51:17 -0500

    redux conectado con componente card

--- Client/src/components/Ciudades/Ciudades.tsx
+++ Client/src/components/Ciudades/Ciudades.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect} from "react";
 import { CardCiudad } from "../CardCiudad/CardCiudad";
-import dataCities from "./CiudadesData";
 import styles from "./Ciudades.module.css"
 import { getCities } from "../../redux/actions";
 
@@ -9,15 +8,15 @@ import { useAppDispatch, useAppSelector } from "../../redux/Hooks/Hooks";
 export function Ciudades () {
 
     const dispatch = useAppDispatch()
-    const allCities = useAppSelector((state) => state.cities)
+    const allCities:any = useAppSelector((state) => state.cities)
 
     useEffect(() => {
         dispatch(getCities() as any)
     },[dispatch])
 
     return(<div className={styles.container}>
-        {dataCities.results.length ?
-        dataCities.results.map(c => {
+        {allCities.length ?
+        allCities.map((c:any) => {
             return(
                 <CardCiudad
                     shortName={c.shortName}
