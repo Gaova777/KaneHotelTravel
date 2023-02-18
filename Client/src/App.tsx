07008b5bf98b7a71d4f8bf1f31d228277ec257f7
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-02-18 04:33:13 -0500

    Feat/ Creada ruta: registro de hoteles
    importado por destructuring las view y creada ruta: registro hoteles

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Actividades } from './components/Actividades/Actividades'
 import { Ciudades } from './components/Ciudades/Ciudades'
 import { MainPage } from './components/MainPage/MainPage'
+import { Form } from './view'
 import {Route, Routes} from "react-router-dom"
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Ciudades/>}/>
         <Route path="/MainPage/:cityName" element={<MainPage/>}/>
+        <Route path="/backoffice/hotel/create" element={<Form />} />
       </Routes>
     </div>
   )
