c6df22f438f8cc89424a5b8ff79b657654641949
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-02-19 15:52:24 -0500

    Feat: agregado lista desplegable en el formulario CreateHotel para seleccionar ciudad

--- Client/src/view/CreateHotel/CreateHotel.tsx
+++ Client/src/view/CreateHotel/CreateHotel.tsx
@@ -4,13 +4,17 @@ import { useFormik } from 'formik';
 import * as Yup from "yup";
 import { getCities } from "../../redux/actions";
 import {City, Record} from "../../utils/interfaces";
-import InputText from "../../components/Form/Input/Input";
-import Button from "../../components/Form/Button/Button";
+import {Input, Button} from "../../components/Form/index";
+import styleForm from "../../components/Form/Form.module.css"
 
 const CreateHotel: React.FC = () =>{
   const [record, setRecord] = useState<Record>({})
   const dispatch = useAppDispatch()
   const allCities:Array<City> = useAppSelector((state) => state.cities)
+
+  useEffect(() => {
+    dispatch(getCities() as any)
+  },[dispatch])
 
   const formik = useFormik({
     initialValues: {
@@ -25,6 +29,7 @@ const CreateHotel: React.FC = () =>{
       name: Yup.string().required("Nombre obligatorio")
     }),
     onSubmit: (formData) => {
+      console.log("formData: ",formData);
       
       fetch(`${import.meta.env.VITE_URL_BACKEND}/hotel`, {
         method: "POST",
@@ -45,11 +50,6 @@ const CreateHotel: React.FC = () =>{
     },
   });
 
-  useEffect(() => {
-    dispatch(getCities() as any)
-  },[dispatch])
-
-
   return (
     record.registered === true
       ? <p>Vamos a cargar el componente HotelDetail {JSON.stringify(record.response)} </p>
@@ -57,7 +57,7 @@ const CreateHotel: React.FC = () =>{
     <div>
       <form onSubmit={formik.handleSubmit}>
         {record.error && <h2>{record.error}</h2>}
-        <InputText
+        <Input
           name="name"
           type="text"
           placeholder="Nombre(*): "
@@ -65,7 +65,7 @@ const CreateHotel: React.FC = () =>{
           value={formik.values.name}
           error={formik.errors.name}
         />
-        <InputText
+        <Input
           name="image"
           type="text"
           placeholder="Imagen"
@@ -73,7 +73,7 @@ const CreateHotel: React.FC = () =>{
           value={formik.values.image}
           error={formik.errors.image}
         />
-        <InputText
+        <Input
           name="services"
           type="text"
           placeholder="Servicios"
@@ -81,7 +81,7 @@ const CreateHotel: React.FC = () =>{
           value={formik.values.services}
           error={formik.errors.services}
         />
-        <InputText
+        <Input
           name="description"
           type="text"
           placeholder="Descripción"
@@ -89,7 +89,7 @@ const CreateHotel: React.FC = () =>{
           value={formik.values.description}
           error={formik.errors.description}
         />
-        <InputText
+        <Input
           name="category"
           type="text"
           placeholder="Categoría"
@@ -97,6 +97,15 @@ const CreateHotel: React.FC = () =>{
           value={formik.values.category}
           error={formik.errors.category}
         />
+        <div>
+          <select name="city" onChange={formik.handleChange} value={formik.values.city}>
+            <option value="">Ciudad</option>
+            {allCities.map(cityDb => (
+              <option value={cityDb.name} key={cityDb.id}>{cityDb.name}</option>
+            ))}
+          </select>
+          <p className={styleForm.errorMessage}>{formik.errors.city && formik.errors.city}</p>
+        </div>
 
         <Button type="submit" value="Registrar" />
         <Button type="reset" value="Limpiar" onClick={formik.handleReset} />
