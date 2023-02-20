c121d4aa5423817d8c8be58c4f2e456c7bf1b8df
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-02-19 21:50:47 -0500

    Feat: Agregada barra de navegacion en MainPage

--- Client/src/components/MainPage/MainPage.tsx
+++ Client/src/components/MainPage/MainPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Actividades } from "../Actividades/Actividades";
 import styles from "./MainPage.module.css"
 import { Link, useParams } from "react-router-dom";
+import { CreateNav } from "../Form";
 
 
 export function MainPage(){
@@ -9,6 +10,7 @@ export function MainPage(){
     const params:any = useParams()
 
     return(<div className={styles.container}>
+        <CreateNav value="Nuevo hotel" url="/backoffice/hotel/create"/>
         {/* <h1>Main Page</h1> */}
         <div className={styles.cityName}>
             <h1>{params.cityName}</h1>
